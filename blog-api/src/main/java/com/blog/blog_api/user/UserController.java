ba329fc91d103e473ed8060e15ce496cebefd4d9
mayhrem <renecruz.0711@gmail.com>
2025-01-05 21:09:06 -0600

    Adding REST code for user

--- blog-api/src/main/java/com/blog/blog_api/user/UserController.java
+++ blog-api/src/main/java/com/blog/blog_api/user/UserController.java
@@ -1,5 +1,51 @@
 package com.blog.blog_api.user;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+  @Autowired
+  private UserService service;
+
+  @GetMapping
+  public List<User> getAllUsers () {
+    return service.getAllUsers();
+  }
+
+  @PostMapping
+  public User createUser (@RequestBody User user) {
+    return service.createUser(user);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<User> getUserById (@PathVariable Long id) {
+    return service.getUserById(id);
+  }
+
+  @PutMapping("/{id}")
+  public User updateUser (@PathVariable Long id, @RequestBody User user) {
+    return service.updateUser(id, user);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteUser (@PathVariable Long id) {
+    service.deleteUser(id);
+  }
+  
 }
--- blog-api/src/main/java/com/blog/blog_api/user/UserService.java
+++ blog-api/src/main/java/com/blog/blog_api/user/UserService.java
@@ -1,5 +1,39 @@
 package com.blog.blog_api.user;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.sql.Update;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+  
+  @Autowired
+  private UserRepository repository;
+
+  public List<User> getAllUsers () {
+    return repository.findAll();
+  }
+
+  public User createUser (User user) {
+    return repository.save(user);
+  }
+
+  public ResponseEntity<User> getUserById (Long id) {
+    Optional<User> user = repository.findById(id);
+    return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  public User updateUser (Long id, User user) {
+    user.setId(id);
+    return repository.save(user);
+  }
+
+  public void deleteUser (Long id) {
+    repository.deleteById(id);
+  }
 
 }
