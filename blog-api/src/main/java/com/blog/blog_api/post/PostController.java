7a65f4966a0fc9a53355c85bf374410e6ec6eada
mayhrem <renecruz.0711@gmail.com>
2025-01-05 21:08:45 -0600

    Adding REST code

--- blog-api/src/main/java/com/blog/blog_api/post/PostController.java
+++ blog-api/src/main/java/com/blog/blog_api/post/PostController.java
@@ -1,5 +1,47 @@
 package com.blog.blog_api.post;
 
-public class PostController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+  @Autowired
+  private PostService service;
+
+  @GetMapping
+  public List<Post> getAllPosts () {
+    return service.getAllPosts();
+  }
+
+  @PostMapping
+  public Post createPost (@RequestBody Post post) {
+    return service.createPost(post);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Post> getPostById (@PathVariable Long id) {
+    return service.getPostById(id);
+  }
+
+  @PutMapping("/{id}")
+  public Post updatePost (@PathVariable Long id, @RequestBody Post post) {
+    return service.updatePost(id, post);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deletePost(@PathVariable Long id) {
+    service.deletePost(id);
+  }
 }
--- blog-api/src/main/java/com/blog/blog_api/post/PostService.java
+++ blog-api/src/main/java/com/blog/blog_api/post/PostService.java
@@ -1,5 +1,36 @@
 package com.blog.blog_api.post;
 
-public class PostService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+  @Autowired
+  private PostRepository repository;
+
+  public List<Post> getAllPosts () {
+    return repository.findAll();
+  }
+
+  public Post createPost (Post post) {
+    return repository.save(post);
+  }
+
+  public ResponseEntity<Post> getPostById (Long id) {
+    Optional<Post> post = repository.findById(id);
+    return post.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  public Post updatePost (Long id, Post post) {
+    post.setId(id);
+    return repository.save(post);
+  }
+
+  public void deletePost (Long id) {
+    repository.deleteById(id);
+  }
 }
